09cd2343dfcce963a6bbcfdf25c9546502bc00a2
pavani <lalithaPavani24893@gmail.com>
2023-06-27 18:28:19 +0530

    commit

---
+++ APMT_Automation/src/main/java/testBase/ObjectsRepo.java
@@ -0,0 +1,17 @@
+package testBase;
+
+
+
+
+import pageObjects.HomePageObjects;
+import pageObjects.LoginPageObjects;
+import pageObjects.SalesManagerObjects;
+import reusableComponents.CommonMethods;
+
+public class ObjectsRepo {
+	
+	public HomePageObjects homePageObjects ;
+	public LoginPageObjects loginPageObjects ;
+	public SalesManagerObjects salesManagerObjects;
+	public CommonMethods commonMethods = new CommonMethods();
+}
